c580848227d7a5f25fe348b21c663f0abec535e4
atik94 <atikurrahmanatik94@gmail.com>
2023-10-22 00:32:23 +0600

    Error page design

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,29 @@
 const Error = () => {
+  const divStyle = {
+    backgroundImage: 'url("https://i.ibb.co/1d9fJVJ/brett-jordan-XWar9-Mb-NGUY-unsplash.jpg")',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div>
-      <h2>Error page</h2>
-    </div>
+    <section className=" h-screen" style={divStyle}>
+      <div className="bg-black opacity-80 h-screen">
+        <div className="flex flex-col justify-center items-center myComponent pt-32">
+          <div className="">
+            <img
+              className="w-28 h-28 rounded-full"
+              src="https://i.ibb.co/GJZRbRp/mulyadi-Zn-Lpr-In-KM7s-unsplash.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <h3 className="text-[#26BCE7] mt-10 text-5xl">Opps 404</h3>
+          </div>
+          <div className="mt-20 animate-bounce transition duration-1000 ease-in-out">
+            <p className="text-3xl font-bold text-[#26BCE7]">Page not found</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
