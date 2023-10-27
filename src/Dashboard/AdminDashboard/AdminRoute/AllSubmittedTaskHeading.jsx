f7ea8a222d7dd19a4ea28b0f45b0c815c4aef146
Yusuf Bin Imam <108363841+yusuf-bin-imam@users.noreply.github.com>
2023-10-27 22:58:13 +0600

    Update AllSubmittedTaskHeading.jsx

--- src/Dashboard/AdminDashboard/AdminRoute/AllSubmittedTaskHeading.jsx
+++ src/Dashboard/AdminDashboard/AdminRoute/AllSubmittedTaskHeading.jsx
@@ -1,17 +1,18 @@
-const AllSubmittedTaskHeading = ({task, Heading, numberHeading}) => {
-    return (
-      <div>
-        <div className="md:flex text-center  justify-between items-center  p-5 pb-5">
-          <div>
-            <h1 className="text-2xl font-semibold">{Heading}</h1>
-          </div>
-          <div>
-          <p className="text-xl font-semibold">{numberHeading} : {task}</p>
-          </div>
+const AllSubmittedTaskHeading = ({ task, Heading, numberHeading }) => {
+  return (
+    <div>
+      <div className=" text-start    p-5 pb-5">
+        <div>
+          <h1 className="text-3xl title">{Heading}</h1>
+        </div>
+        <div>
+          <p className="text-md font-semibold">
+            {numberHeading} : {task}
+          </p>
         </div>
       </div>
-    );
-  };
-  
-  export default AllSubmittedTaskHeading;
-  
+    </div>
+  );
+};
+
+export default AllSubmittedTaskHeading;
