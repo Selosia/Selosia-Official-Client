54dd19bc8c58d9ff48222642ee7c7b079c87a1d5
Yusuf Bin Imam <108363841+yusuf-bin-imam@users.noreply.github.com>
2023-10-28 01:21:04 +0600

    some chages added on Order Now section

--- src/Dashboard/DashboardShared/OrderNow.jsx
+++ src/Dashboard/DashboardShared/OrderNow.jsx
@@ -5,68 +5,73 @@ import { orderNewProject } from "../../apis/orderOperation";
 import { handleImgBBUpload } from "../../apis/imageUploadOperatio";
 
 const OrderNow = () => {
+  const [convertedDatetime, setConvertedDatetime] = useState("");
+  const [isUploading, setUploading] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const [convertedDatetime, setConvertedDatetime] = useState('');
+    const form = e.target;
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const form = e.target;
-
-        const projectName = form.projectName.value;
-        const buyerEmail = form.buyerEmail.value;
-        const buyerName = form.buyerName.value;
-        const domainName = form.domainName.value;
-        const category = form.category.value;
-        const description = form.description.value;
-        const image = form.image.files[0];
-        const OrderDate = String(new Date().toLocaleString());
-
-        handleImgBBUpload(image)
-        .then((res) => {
-        const imageUrl = res
+    const projectName = form.projectName.value;
+    const buyerEmail = form.buyerEmail.value;
+    const buyerName = form.buyerName.value;
+    const domainName = form.domainName.value;
+    const category = form.category.value;
+    const description = form.description.value;
+    const image = form.image.files[0];
+    const OrderDate = String(new Date().toLocaleString());
+    setUploading(true);
+    handleImgBBUpload(image)
+      .then((res) => {
+        const imageUrl = res;
 
         console.log("imageUrl", imageUrl);
 
         const orderInfo = {
-            projectName,
-            orderSenderEmail: buyerEmail,
-            orderSenderName: buyerName,
-            domainName,
-            category,
-            description,
-            imageUrl,
-            status: "pending",
-            OrderDate,
-            deliveryDate : convertedDatetime,
+          projectName,
+          orderSenderEmail: buyerEmail,
+          orderSenderName: buyerName,
+          domainName,
+          category,
+          description,
+          imageUrl,
+          status: "pending",
+          OrderDate,
+          deliveryDate: convertedDatetime,
         };
 
-        orderNewProject(orderInfo)
-        .then((response)=>{
-            console.log(response)
-        })
-        })
-        .catch((err) => console.log(err));
-    };
+        orderNewProject(orderInfo).then((response) => {
+          console.log(response);
+          form.reset();
+        });
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setUploading(false);
+      });
+  };
 
-
-    return (
-        <div>
-            <div>
-                <section>
-                <div className="p-8">
-                    <h1 className="text-2xl font-semibold text-center">Order Now</h1>
-                </div>
-                <div className="mx-auto max-w-[600px] px-4  pb-8 sm:px-6 lg:px-8">
-                    <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-                    <OrderNowInputField handleSubmit={handleSubmit} setConvertedDatetime= {setConvertedDatetime} />
-                    </div>
-                </div>
-                </section>
+  return (
+    <div>
+      <div>
+        <section>
+          <div className="p-8">
+            <h1 className="text-2xl font-semibold text-center">Order Now</h1>
+          </div>
+          <div className="mx-auto max-w-[600px] px-4  pb-8 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+              <OrderNowInputField
+                isUploading={isUploading}
+                handleSubmit={handleSubmit}
+                setConvertedDatetime={setConvertedDatetime}
+              />
             </div>
-        </div>
-    )};
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default OrderNow;
--- src/apis/orderOperation.jsx
+++ src/apis/orderOperation.jsx
@@ -1,139 +1,129 @@
+import Swal from "sweetalert2";
 import instance from "../axios";
 
-
-
-
-//* .......... Save a new order .......... 
+//* .......... Save a new order ..........
 
 export const orderNewProject = async (orderInfo) => {
-    try{
-        const response = await instance.post('api/v1/project/create-orderedProject',orderInfo )
-        const data = await response;
-        console.log(data);
-        return data;
-    } 
-    catch (error) {
-        console.log(error.message);
+  try {
+    const response = await instance.post(
+      "api/v1/project/create-orderedProject",
+      orderInfo
+    );
+    const data = await response;
+    console.log(data);
+    if (data?.status === "success") {
+      Swal.fire("Wow😲", "Order Successfully Submitted", "success");
     }
-}
+    return data;
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops👎",
+      text: "Something went wrong!",
+    });
+    console.log(error.message);
+  }
+};
 
-
-
-//* .......... Get all pending orders .......... 
+//* .......... Get all pending orders ..........
 
 export const getAllPendingOrders = async () => {
-    try {
-        const response = await instance.get('api/v1/project/all-pendingOrders');
-        const data = await response;
-        console.log(data);
-        return data?.data;
-        } 
-    catch (error) {
-        console.log(error.message);
-        }
+  try {
+    const response = await instance.get("api/v1/project/all-pendingOrders");
+    const data = await response;
+    console.log(data);
+    return data?.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-
-
-//* .......... handle order request approve .......... 
+//* .......... handle order request approve ..........
 
 export const approveOrder = async (orderId, updateOrder) => {
-    try {
-        const response = await instance.put(`api/v1/project/update-orderStatus/${orderId}`, updateOrder);
-        console.log(response)
-        return response;
-        } catch (error) {
-        console.log(error.message);
-        }
-    };
+  try {
+    const response = await instance.put(
+      `api/v1/project/update-orderStatus/${orderId}`,
+      updateOrder
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
+//* .......... Update user role by clicking approve button ..........
 
+export const orderSenderRoleUpdate = async (
+  orderSenderEmail,
+  updateBuyerRole
+) => {
+  try {
+    const response = await instance.put(
+      `api/v1/user/role-update?email=${orderSenderEmail}`,
+      updateBuyerRole
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-//* .......... Update user role by clicking approve button .......... 
-
-export const orderSenderRoleUpdate = async (orderSenderEmail, updateBuyerRole) => {
-    try {
-        const response = await instance.put(`api/v1/user/role-update?email=${orderSenderEmail}`, updateBuyerRole);
-        console.log(response)
-        return response;
-        } catch (error) {
-        console.log(error.message);
-        }
-    };
-
-
-
-
-
-
-//* .......... Get all approve orders .......... 
+//* .......... Get all approve orders ..........
 
 export const getAllApproveProjects = async () => {
-    try {
-        const response = await instance.get('api/v1/project/all-approvedOrders');
-        const data = await response;
-        console.log(data);
-        return data?.data;
-        } 
-    catch (error) {
-        console.log(error.message);
-        }
+  try {
+    const response = await instance.get("api/v1/project/all-approvedOrders");
+    const data = await response;
+    console.log(data);
+    return data?.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-
-
-
-
-
-//* .......... Get all working orders .......... 
+//* .......... Get all working orders ..........
 
 export const getAllWorkingProjects = async () => {
-    try {
-        const response = await instance.get('api/v1/project/all-workingOrders');
-        const data = await response;
-        console.log(data);
-        return data?.data;
-        } 
-    catch (error) {
-        console.log(error.message);
-        }
+  try {
+    const response = await instance.get("api/v1/project/all-workingOrders");
+    const data = await response;
+    console.log(data);
+    return data?.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
+//* .......... Update project Contributors by clicking set Contributors button ..........
 
+export const projectContributorsUpdate = async (id, addedDevContributors) => {
+  try {
+    const response = await instance.put(
+      `api/v1/project/update-contributors/${id}`,
+      addedDevContributors
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-
-
-
-//* .......... Update project Contributors by clicking set Contributors button .......... 
-
-export const projectContributorsUpdate = async (id,addedDevContributors) => {
-    try {
-        const response = await instance.put(`api/v1/project/update-contributors/${id}`, addedDevContributors);
-        console.log(response)
-        return response;
-        } catch (error) {
-        console.log(error.message);
-        }
-    };
-
-
-
-
-
-
-
-
-
-//* .......... send email a to all selected project developers  .......... 
+//* .......... send email a to all selected project developers  ..........
 
 export const sendSelectedProjectDevelopers = async (mailInfo) => {
-    try {
-        const response = await instance.post('api/v1/sendMail/mailToContributor', mailInfo);
-        const data = await response;
-        console.log(data);
-        return data?.data;
-        } 
-    catch (error) {
-        console.log(error.message);
-        }
+  try {
+    const response = await instance.post(
+      "api/v1/sendMail/mailToContributor",
+      mailInfo
+    );
+    const data = await response;
+    console.log(data);
+    return data?.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
