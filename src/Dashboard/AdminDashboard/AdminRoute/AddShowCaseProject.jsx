f4bf0464405f92e627c1098ce66ae4dea3338788
Yusuf Bin Imam <108363841+yusuf-bin-imam@users.noreply.github.com>
2023-10-27 22:55:21 +0600

    Update AddShowCaseProject.jsx

--- src/Dashboard/AdminDashboard/AdminRoute/AddShowCaseProject.jsx
+++ src/Dashboard/AdminDashboard/AdminRoute/AddShowCaseProject.jsx
@@ -3,10 +3,9 @@ import AddShowCaseProjectField from "./AddShowCaseProjectField";
 const AddShowCaseProject = () => {
   return (
     <section>
-      <div className="p-8">
-        <h1 className="text-2xl font-semibold text-center">
-          Add Showcase Project
-        </h1>
+      <div className="p-8 text-center">
+        <h1 className="text-3xl title ">Add Showcase Project</h1>
+        <p className="font-semibold mt-2">Teamwork in Every Line of Code</p>
       </div>
 
       <div className="mx-auto max-w-[600px] px-4  pb-8 sm:px-6 lg:px-8">
